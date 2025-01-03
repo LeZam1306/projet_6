30a9e71a5dc20ae5145af28a7fee4aa57d7d3c21
Adam Zamouri <adamzamouricontact@gmail.com>
2025-01-03 14:56:40 +0100

    Added dynamic filter system

--- FrontEnd/js/api.js
+++ FrontEnd/js/api.js
@@ -1,12 +1,11 @@
-//Function for get works data in the api
-export async function getWorks(){
+//function for get any elements
+async function getAny(request){
     try{
-        let reponse = await fetch("http://localhost:5678/api/works");
+        let reponse = await fetch(request);
         if (!reponse.ok){
-            throw new Error(`Error ${reponse.status} for api/works`);
+            throw new Error(`Error ${reponse.status} for ${request}`);
         }else{
             let data = await reponse.json();
-            //console.log(data);
             return data;
         }
     }catch(error)
@@ -14,3 +13,11 @@ export async function getWorks(){
         console.log(`Message : ${error.message}`);
     }
 }
+//Function for get works data in the api
+export async function getWorks(){
+   return await getAny("http://localhost:5678/api/works");
+}
+//function for get caterogies
+export async function getCategories(){
+    return await getAny("http://localhost:5678/api/categories");
+ }
--- FrontEnd/js/utils.js
+++ FrontEnd/js/utils.js
@@ -1,12 +1,25 @@
 //Function for create a template element of the galery
-export function createElementGallery(imageSrc,title,id){
+function createElementGallery(imageSrc,title,id){
     return `
-        <figure>
-            <img data-id="${id}" src="${imageSrc}" alt="${title}">
+        <figure data-id="${id}">
+            <img src="${imageSrc}" alt="${title}">
             <figcaption>${title}</figcaption>
         </figure>
     `;
 }
-export function createElementFilter(){
-    
+//function for create a filter item
+export function createElementFilter(parent,name){
+    let filterElement = document.createElement("div");
+    filterElement.classList.add("filter__item");
+    filterElement.dataset.name = name;
+    filterElement.innerText = name;
+    parent.appendChild(filterElement);
+}
+//dynamic display of elements in the page
+export function dynamicDisplayGallery(array,parent){
+    parent.innerHTML = ""; //Delete preset gallery
+    array.forEach(data => {
+        let element = createElementGallery(data.imageUrl,data.title,data.id);
+        parent.innerHTML += element;
+    });
 }
